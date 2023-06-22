aedbfa2432221927a454cc2397a5c0445862e114
Martin <martyofmca@gmail.com>
2023-06-21 21:07:21 -0700

    run appropriate function for monty opcode

---
+++ _stackoperation.c
@@ -0,0 +1,23 @@
+#include "monty.h"
+
+/**
+ * _stackoperation - Invoke the appropriate function
+ * to handle stack manipulation.
+ *
+ * @operation: Opcode and its function.
+ * @line_number: The line number read.
+ * @head: The head node in the list
+ *
+ * Return: The exit code for the latest stack operation.
+ *
+ **/
+int _stackoperation(operation_t operation, unsigned int line_number
+                , stack_t **head)
+{
+	_global_operation.instruction = operation.instruction;
+	_global_operation.operand = operation.operand;
+	_global_operation.exit_code = operation.exit_code;
+	_global_operation.instruction.f(head, line_number);
+
+	return (*_global_operation.exit_code);
+}

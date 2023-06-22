9336e9441969ce8fe2e2dfdb2f25a018db3d55f7
Ardiy <you@example.com>
2023-06-22 23:48:46 +0000

    compare to integer rather , fix

--- _odiv.c
+++ _odiv.c
@@ -8,39 +8,37 @@
  */
 void _odiv(stack_t **head, unsigned int line_number)
 {
-	stack_t *tmp = NULL;
-	char *itoa = NULL;
-	operation_t operation;
-	int result = 0;
+    stack_t *tmp = NULL;
+    char *itoa = NULL;
+    operation_t operation;
+    int result = 0;
 
-	if ((*head) != NULL && (*head)->next != NULL)
-	{
-		tmp = *head;
-		result = (*head)->n / (*head)->next->n;
-		(*head) = (*head)->next;
-		(*head)->n = result;
-		(*head)->prev = NULL;
-		free(tmp);
-		return;
-	}
+    if ((*head) != NULL && (*head)->next != NULL)
+    {
+        tmp = *head;
+        result = (*head)->next->n;
+        
+        if (result == 0)
+        {
+            operation = _global_operation;
+            itoa = _itoa(line_number, operation.exit_code);
+            _error(operation.exit_code, 2, "L", itoa, ": division by zero");
+            free(itoa);
+            *_global_operation.exit_code = EXIT_FAILURE;
+            return;
+        }
 
-	operation = _global_operation;
-	itoa = _itoa(line_number, operation.exit_code);
-	if (itoa != NULL)
-	{
-		_error(operation.exit_code, 2, "L", itoa,
-		       ": can't div, stack too short");
-		free(itoa);
-		*_global_operation.exit_code = EXIT_FAILURE;
-		return;
-	}
+        result = (*head)->n / result;
+        (*head) = (*head)->next;
+        (*head)->n = result;
+        (*head)->prev = NULL;
+        free(tmp);
+        return;
+    }
 
-	itoa = _itoa(line_number, operation.exit_code);
-	if ((*head)->n == 0)
-	{
-		_error(operation.exit_code, 2, "L", itoa,
-		       ": division by zero");
-		free(itoa);
-		*_global_operation.exit_code = EXIT_FAILURE;
-	}
+    operation = _global_operation;
+    itoa = _itoa(line_number, operation.exit_code);
+    _error(operation.exit_code, 2, "L", itoa, ": can't div, stack too short");
+    free(itoa);
+    *_global_operation.exit_code = EXIT_FAILURE;
 }

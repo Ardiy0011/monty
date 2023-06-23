bd11fc35889b4db4ab25b0f483e0c702e8894ec4
Ardiy <you@example.com>
2023-06-23 00:22:57 +0000

    indentation fix

--- _odiv.c
+++ _odiv.c
@@ -8,37 +8,34 @@
  */
 void _odiv(stack_t **head, unsigned int line_number)
 {
-    stack_t *tmp = NULL;
-    char *itoa = NULL;
-    operation_t operation;
-    int result = 0;
+	stack_t *tmp = NULL;
+	char *itoa = NULL;
+	operation_t operation;
+	int result = 0;
 
-    if ((*head) != NULL && (*head)->next != NULL)
-    {
-        tmp = *head;
-        result = (*head)->next->n;
-        
-        if (result == 0)
-        {
-            operation = _global_operation;
-            itoa = _itoa(line_number, operation.exit_code);
-            _error(operation.exit_code, 2, "L", itoa, ": division by zero");
-            free(itoa);
-            *_global_operation.exit_code = EXIT_FAILURE;
-            return;
-        }
+	if ((*head) != NULL && (*head)->next != NULL)
+	{
+	tmp = *head;
+	result = (*head)->next->n;
+	if (result == 0)
+	{
+		operation = _global_operation;
+		itoa = _itoa(line_number, operation.exit_code);
+		_error(operation.exit_code, 2, "L", itoa, ": division by zero");
+		free(itoa);
+		*_global_operation.exit_code = EXIT_FAILURE;
+		return;
+	}
+	result = (*head)->n / result;
+	(*head) = (*head)->next;
+	(*head)->n = result;
+	(*head)->prev = NULL;
+	free(tmp);
+	return;
+	}
+	operation = _global_operation;
+	itoa = _itoa(line_number, operation.exit_code);
+	_error(operation.exit_code, 2, "L", itoa, ": can't div, stack too short");
 
-        result = (*head)->n / result;
-        (*head) = (*head)->next;
-        (*head)->n = result;
-        (*head)->prev = NULL;
-        free(tmp);
-        return;
-    }
-
-    operation = _global_operation;
-    itoa = _itoa(line_number, operation.exit_code);
-    _error(operation.exit_code, 2, "L", itoa, ": can't div, stack too short");
-
-    *_global_operation.exit_code = EXIT_FAILURE;
+	*_global_operation.exit_code = EXIT_FAILURE;
 }
